81ce73e9edc60aa9a0c6e1ae365aa51428926a75
alekslesik <alekslesik@gmail.com>
2022-06-21 14:10:32 +0300

    osveshchenie-gorodskikh-ulits: create

---
+++ osveshchenie-gorodskikh-ulits.php
@@ -0,0 +1,266 @@
+<style>
+    .ceo-wrap-grey {
+        background-color: #FBFBFB;
+        padding: 40px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-white {
+        padding: 40px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-grey h3 {
+        text-align: center;
+    }
+
+    .ceo-container {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .left {
+        align-items: center;
+    }
+
+    .left img {
+        margin: 0;
+    }
+
+    .compound img {
+        max-width: 200px;
+    }
+
+    .ceo-flex-elem {
+        width: calc((100% - 100px) / 2);
+        margin: 40px 10px 10px 40px;
+    }
+
+    .ceo-flex-elem-2 {
+        display: flex;
+        align-content: center;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+
+    .flex-parent {
+        display: flex;
+        border-radius: 5px;
+    }
+
+    .flex-child {
+        border-radius: 5px;
+        background: #f6f6f6;
+        padding: 15px;
+        box-sizing: border-box;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+    }
+
+    .flex-child:nth-of-type(2) {
+        background: #f1f1f1;
+    }
+
+    .flex-elem span {
+        display: block;
+        font-size: 20px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        font-weight: bold;
+    }
+
+    .ceo-hidden {
+        opacity: 0;
+    }
+
+    .flex-elem li {
+        margin: 5px 0px 0px 0px;
+    }
+</style>
+
+
+<div class="ceo-wrap-grey">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <h4>Свет будущего уже сегодня</h4>
+            <p>Система АСУНО позволяет управлять освещением города от одного компьютера. Узлы умного освещения могут работать в автоматическом режиме. Можно управлять одним или группой светильников. Допускается использование различных каналов связи.</p>
+            <p>Датчики движения контролируют яркость в случае приближения объекта. Диммирование может осуществляться как одного, так и группы светодиодных светильников. Автоматизация управления снижает затраты на обслуживание до 50%.</p>
+        </div>
+        <div class="ceo-flex-elem">
+            <div>
+                <img style="margin: 0px !important;" width="100%" src="/local/templates/aspro-allcorp3_energy/images/osvesh_gor_ulic.png" style="border-radius:5px;">
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <h4>Экономичность и экологичность</h4>
+            <p>Умное освещение работает только тогда, когда это необходимо. Светодиодные светильники оснащаются датчиками движения. Система позволяет регулировать интенсивность света, уменьшая затраты электроэнергии до 60%.</p>
+            <p>В итоге, снижается нагрузка на электростанции. Следовательно, уменьшается выброс СО в атмосферу. Вместо ртутных ламп освещения, используются светодиодные светильники. Почти все элементы системы Spot Light могут проходить повторную переработку.</p>
+        </div>
+        <div class="ceo-flex-elem">
+            <h4>Полный контроль и безопасность</h4>
+            <p>Использование IoT для города позволяет контролировать освещение. На основе данных о городской среде IoT решения позволяют улучшать систему освещения. Выявлять слабые места инфраструктуры и находить проблемные участки.</p>
+            <p>Можно запрограммировать самые сложные схемы освещения по всему городу. Усиливая или уменьшая яркость в определённые часы. Самодиагностика системы Spot Light позволяет оперативно выявлять и решать проблемы с освещением.</p>
+        </div>
+    </div>
+    <h3 style="margin: 60px 0 40px 0;">Система уличного освещения «Spot Light»</h3>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem-2">
+            <div style="padding: 20px 20px 20px 0;">
+                <img style="margin: 0;" src="/local/templates/aspro-allcorp3_energy/images/icon_adv.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px;">
+                Парки
+            </div>
+        </div>
+        <div class="ceo-flex-elem-2">
+            <div style="padding: 20px 20px 20px 0;">
+                <img style="margin: 0;" src="/local/templates/aspro-allcorp3_energy/images/icon_adv.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px;">
+                Городские улицы
+            </div>
+        </div>
+        <div class="ceo-flex-elem-2">
+            <div style="padding: 20px 20px 20px 0;">
+                <img style="margin: 0;" src="/local/templates/aspro-allcorp3_energy/images/icon_adv.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px;">
+                Автомагистрали
+            </div>
+        </div>
+        <div class="ceo-flex-elem-2">
+            <div style="padding: 20px 20px 20px 0;">
+                <img style="margin: 0;" src="/local/templates/aspro-allcorp3_energy/images/icon_adv.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px;">
+                Логистические центры
+            </div>
+        </div>
+    </div>
+</div>
+<div class="ceo-wrap-white">
+    <h2>Основные звенья Spot Light</h2>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <p> В состав системы входит четыре главных звена: светодиодные светильники, датчики движения, контроллеры и шкаф управления. Все звенья управляются программой. Она отражает их деятельность, и анализирует состояние.
+            </p>
+            <p> В свою очередь программа позволяет:
+            </p>
+            <ul>
+                <li>
+                    &mdash; учитывать расход электричества;
+                </li>
+                <li> &mdash; регулировать параметры (яркость) освещения;
+                </li>
+                <li> &mdash; дистанционно управлять освещением;
+                </li>
+                <li> &mdash; контролировать работу датчиков движения.
+                </li>
+            </ul>
+        </div>
+        <div class="ceo-flex-elem">
+            <p> Умное освещение производственных площадок – это использование энергии только по необходимости. Датчики движения позволяют снизить/повысить освещённость, когда это нужно. Программа автоматически выключает/включает свет согласно заданной команде.
+            </p>
+            <p> В целом, система АСУНО Spot Light – удобный, экономичный и полезный инструмент. Это выгодное вложение, которое окупается за счёт существенного снижения расходов. «Спот Лайт» – это доступный свет на безопасных и комфортных условиях!
+            </p>
+        </div>
+    </div>
+</div>
+<div class="ceo-wrap-grey">
+    <h2>Функционал оборудования</h2>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <h4>Spot Light 1.0</h4>
+            <p>Контролер управления размещается непосредственно на светильнике, а управляемый драйвер в корпусе. Подключение контроллера к светильнику осуществляется по протоколу DALI, разъем Zhaga.
+                Связь шкафа управления наружным освещением (ШУНО) с контроллером организована по радиоканалу 2,4ГГц, между контроллерами по ретрансляции сигнала.
+                Данное решение имеет обширный опыт применения, за счет простоты монтажа и надежности.</p>
+        </div>
+    </div>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <h4>Spot Light 2.0</h4>
+            <p>
+                Размещение контроллера с драйвером в одном корпусе на опоре освещения или внутри полой композиции опоры.
+                Связь шкафа управления наружным освещением с контроллером организованна по WIFI, между контроллерами также по ретрансляции. Дополнительно, контроллеры оборудованы модулями часов, позволяет каждому светильнику работать в автономном режиме, даже в случае обрыва связи с ШУНО.
+                Данное решение позволяет производить обслуживание оборудования без автовышки и подключать к одному контроллеру до трех светильников одновременно, что позволяет экономить на стоимости оборудования и обслуживания в дальнейшем.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="ceo-wrap-white">
+    <h2>В состав системы «Spot Light» входят:</h2>
+    <div class="ceo-container compound">
+        <div class="ceo-flex-elem-composition">
+            <div>
+                <img src="/local/templates/aspro-allcorp3_energy/images/svetodiod_svetiln.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px; text-align: center;">
+                Светодиодные светильники
+            </div>
+        </div>
+        <div class="ceo-flex-elem-composition">
+            <div>
+                <img src="/local/templates/aspro-allcorp3_energy/images/shkaf_uprav.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px; text-align: center;">
+                Шкаф управления
+            </div>
+        </div>
+        <div class="ceo-flex-elem-composition">
+            <div>
+                <img src="/local/templates/aspro-allcorp3_energy/images/datchik_dvizh.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px; text-align: center;">
+                Датчики движения
+            </div>
+        </div>
+
+        <div class="ceo-flex-elem-composition">
+            <div>
+                <img src="/local/templates/aspro-allcorp3_energy/images/kontroller_uprav.png" alt="">
+            </div>
+            <div style="padding: 20px 0 20px 0; font-size: 18px; text-align: center;">
+                Контроллеры управления
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="ceo-wrap-grey">
+    <h2>Управление системой</h2>
+    <div class="ceo-container left">
+        <div class="ceo-flex-elem">
+            <div>
+                <img width="100%" src="/local/templates/aspro-allcorp3_energy/images/automatisation.png" style="border-radius:5px;">
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <h5>Яркость</h5>
+            <p>
+                Доммирование яркости света как отдельного светильника, так и линии светильников
+            </p>
+            <h5>Автоматизация</h5>
+            <p>
+                Коммерческий учет энергопотребления.
+            </p>
+            <h5>Трансформация</h5>
+            <p>
+                Изменение уровня яркости светильника по датчику движения
+            </p>
+            <h5>Универсальность</h5>
+            <p>
+                Дистанционное управление освещением с любого устройства с возможностью выхода в интернет
+            </p>
+            <h5>Регулирование</h5>
+            <p>
+                Включение/выключение в соответствии с заданными параметрами (по таблице рассветов и закатов)
+            </p>
+        </div>
+    </div>
+</div>
