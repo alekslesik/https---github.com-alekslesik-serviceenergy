8a48f066c5d33eb528833c7f9e3c320215e1b3a4
alekslesik <alekslesik@gmail.com>
2022-06-22 16:28:50 +0300

    okhrannyy-modul-lep: create

---
+++ okhrannyy-modul-lep.php
@@ -0,0 +1,103 @@
+<style>
+    .ceo-wrap-grey {
+        background-color: #FBFBFB;
+        padding: 40px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-white {
+        padding: 40px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-grey h3 {
+        text-align: center;
+    }
+
+    .ceo-container {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .ceo-flex-elem {
+        width: calc((100% - 100px) / 2);
+        /* margin: 40px 10px 10px 40px; */
+    }
+
+    .ceo-flex-elem-2 {
+        display: flex;
+        align-content: center;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+
+    .ceo-center {
+        text-align: center;
+    }
+
+    .caption {
+        text-align: center;
+
+    }
+
+    .size_w500 {
+        max-width: 500px;
+    }
+
+    .size_h300 {
+        max-height: 400px;
+    }
+
+
+</style>
+
+
+<div class="ceo-wrap-grey">
+    <h3>Отчет по опытной эксплуатации Охранного модуля на ВЛ.</h3>
+    <p>Система была введена в эксплуатацию 27 декабря 2021 года. Монтаж информационных блоков произведен на высоте порядка 6м, на нижнем ярусе опоры Рис.1.</p>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <div class="ceo-center">
+                <img class="size_w600 size_h300"  src="/local/templates/aspro-allcorp3_energy/images/ohr1.png" style="border-radius:5px;">
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <div class="ceo-center">
+                <img class="size_w600 size_h300"  src="/local/templates/aspro-allcorp3_energy/images/ohr2.png" style="border-radius:5px;">
+            </div>
+        </div>
+    </div>
+    <p class="caption">Рис.1 Внешний вид установленной системы видеонаблюдения.</p>
+    <br>
+    <p>В настоящее время проведена настройка датчиков движения в соответствии с условиями эксплуатации. Выполнена настройка камер видеонаблюдения, а также проведена наладка передачи данных с охраняемого объекта. Результаты работы системы можно увидеть в web-сервисе, в котором выводятся все возникающие события Рис.2,3,4.</p>
+    <br>
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <div class="ceo-center">
+                <img class="size_w500" width="100%" src="/local/templates/aspro-allcorp3_energy/images/ohr3.png" style="border-radius:5px;">
+                <p>
+                    Рис.2 Детекция движения в охраняемой зоне.
+                </p>
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <div class="ceo-center">
+                <img class="size_w500" width="100%" src="/local/templates/aspro-allcorp3_energy/images/ohr4.png" style="border-radius:5px;">
+                <p>
+                    Рис.3 Детекция движения в охраняемой зоне.
+                </p>
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <div class="ceo-center">
+                <img class="size_w500" width="100%" src="/local/templates/aspro-allcorp3_energy/images/ohr5.png" style="border-radius:5px;">
+                <p>
+                    Рис.4 Детекция движения в охраняемой зоне.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
