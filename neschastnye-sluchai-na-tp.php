ff36ff33fbb9b0ba644f18bece2b5bb4f8d1050d
alekslesik <alekslesik@gmail.com>
2022-06-24 13:06:46 +0300

    neschastnye-sluchai-na-tp: create

---
+++ neschastnye-sluchai-na-tp.php
@@ -0,0 +1,157 @@
+<style>
+    .ceo-wrap-grey {
+        background-color: #FBFBFB;
+        padding: 20px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-white {
+        padding: 40px 20px 20px 20px;
+        border-radius: 5px;
+        margin-bottom: 48px;
+    }
+
+    .ceo-wrap-grey h3 {
+        text-align: center;
+    }
+
+    .ceo-container {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .left {
+        align-items: center;
+    }
+
+    .left img {
+        margin: 0;
+    }
+
+    .compound img {
+        max-width: 200px;
+    }
+
+    .ceo-flex-elem {
+        width: calc((100% - 100px) / 2);
+        margin: 40px 10px 10px 40px;
+    }
+
+    .ceo-flex-elem-w100 {
+        width: 100%;
+    }
+
+    .ceo-flex-elem-2 {
+        display: flex;
+        align-content: center;
+        flex-wrap: nowrap;
+        align-items: center;
+    }
+
+    .flex-parent {
+        display: flex;
+        border-radius: 5px;
+    }
+
+    .flex-child {
+        border-radius: 5px;
+        background: #f6f6f6;
+        padding: 15px;
+        box-sizing: border-box;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+    }
+
+    .flex-child:nth-of-type(2) {
+        background: #f1f1f1;
+    }
+
+    .flex-elem span {
+        display: block;
+        font-size: 20px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        font-weight: bold;
+    }
+
+    .ceo-hidden {
+        opacity: 0;
+    }
+
+    .flex-elem li {
+        margin: 5px 0px 0px 0px;
+    }
+
+    .t-center {
+        text-align: center;
+    }
+
+</style>
+
+
+<div class="ceo-wrap-grey">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem">
+            <p>Целью разработки данного оборудования является обеспечение дополнительной системы оповещения о наличии напряжения при работах на электроустановках. Электромонтеры, производящие отключение ТП, совершают следующие ошибки:</p>
+            <ul>
+                <li>1. В случаях когда в момент отключения у разъединителей ломались опорные изоляторы и ножи поврежденной фазы не выходили из губок, электромонтеры, не замечая поломку (дефекты), обращали внимание только на положение рукоятки и не обнаруживали не отключенного ножа.</li>
+                <li>2. В случаях, когда ранее выявленные неполадки на отдельных фазах разъединителей (подгорание контактов и др. ) своевременно не устранялись, а ножи разъединителей этих фаз были зашунтированы, электромонтеры, производящие операции, не зная об установке перемычек, ошибочно считали, что с участков ВЛ и ТП после разъединителей напряжение снято.</li>
+            </ul>
+        </div>
+        <div class="ceo-flex-elem t-center">
+            <div>
+                <img style="margin: 0px !important; max-width: 200px;" width="100%" src="/local/templates/aspro-allcorp3_energy/images/tp12.png" style="border-radius:5px;">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="ceo-wrap-white">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem-w100 t-center">
+            <p>
+                Опорные изоляторы 6/10кВ со встроенным емкостным делителем, монтируются на место штатных опорных изоляторов в РУ–6-10кВ. Блок индикации устанавливается на лицевой части ТП.
+                Мигающий индикатор свидетельствует о неисправности (неотключении фазы) коммутационного аппарата 10кВ или другого оборудования, либо о наличии шунтирующих перемычек на коммутационных аппаратах (оборванных проводниках, кратковременно подающих напряжение на обесточенные участки сети), если после выполнения оперативных переключений на отключенной КТП остается напряжение, о чем сигнализирует свечение элементов индикации и сопутствующий звуковой сигнал при открытии дверцы.
+                Индикатор наличия напряжения не нуждается в дополнительном питании.
+            </p>
+        </div>
+    </div>
+</div>
+<div class="ceo-wrap-white">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem t-center">
+            <div>
+                <img style="margin: 0px !important;" width="100%" src="/local/templates/aspro-allcorp3_energy/images/tp2.png" style="border-radius:5px;">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="ceo-wrap-white">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem-w100 t-center">
+            <p>
+                Применение световой и звуковой индикации (сигнализации) позволяет сделать еще одно, дополнительное предупреждение персоналу об опасности, присутствующей в ТП, несмотря на уверенность в отсутствии напряжения, появившуюся у сотрудника после выполненных им операций по отключению оборудования.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="ceo-wrap-grey">
+    <div class="ceo-container">
+        <div class="ceo-flex-elem t-center">
+            <div>
+                <img style="margin: 0px !important; max-width: 400px;" width="100%" src="/local/templates/aspro-allcorp3_energy/images/tp3.png" style="border-radius:5px;">
+            </div>
+        </div>
+        <div class="ceo-flex-elem">
+            <div>
+                <img style="margin: 0px !important; max-width: 400px;" width="100%" src="/local/templates/aspro-allcorp3_energy/images/tp4.png" style="border-radius:5px;">
+            </div>
+        </div>
+    </div>
+</div>
